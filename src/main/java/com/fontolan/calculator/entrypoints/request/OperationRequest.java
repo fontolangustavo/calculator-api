b1fe6d82abad4b3c5320112184c6af8d8484fbe9
Fontolan <fontolangustavo@gmail.com>
2025-08-06 15:33:58 -0300

    fix: operations array validation

--- src/main/java/com/fontolan/calculator/entrypoints/request/OperationRequest.java
+++ src/main/java/com/fontolan/calculator/entrypoints/request/OperationRequest.java
@@ -1,9 +1,7 @@
 package com.fontolan.calculator.entrypoints.request;
 
 import com.fontolan.calculator.domain.enums.OperationType;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,7 +18,5 @@ public class OperationRequest {
     @NotNull(message = "Operation type cannot be null")
     private OperationType type;
 
-    @NotEmpty(message = "Operands list cannot be empty")
-    @Size(min = 1, message = "At least one operand is required")
     private List<@NotNull(message = "Operand cannot be null") BigDecimal> operands;
 }
